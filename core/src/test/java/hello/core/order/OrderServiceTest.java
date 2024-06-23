aee33a9ba19019da051af8ccc38ab485aea34cb0
kingjinho <jinhoshin319@gmail.com>
2024-06-23 21:32:36 +0900

    객체 생성 담당 클래스(AppConfig) 추가에 따른 테스트 @beforeEach 추가

--- core/src/test/java/hello/core/order/OrderServiceTest.java
+++ core/src/test/java/hello/core/order/OrderServiceTest.java
@@ -1,16 +1,26 @@
 package hello.core.order;
 
+import hello.core.AppConfig;
 import hello.core.member.EGrade;
 import hello.core.member.IMemberService;
 import hello.core.member.Member;
 import hello.core.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class OrderServiceTest {
 
-    IMemberService memberService = new MemberServiceImpl();
-    IOrderService orderService = new OrderServiceImpl();
+
+    IMemberService memberService;
+    IOrderService orderService;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+        orderService = appConfig.orderService();
+    }
 
     @Test
     void createOrder() {
