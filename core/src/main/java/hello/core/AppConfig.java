eeb840f95185c99a35fb19d2ee1cae4fed5749d3
kingjinho <jinhoshin319@gmail.com>
2024-06-23 21:39:59 +0900

    MemberRepository, DiscountPoilcy 객체 생성 부분 함수 추출

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -1,22 +1,32 @@
 package hello.core;
 
 import hello.core.discount.FixedDiscountPolicy;
+import hello.core.discount.IDiscountPolicy;
 import hello.core.member.IMemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.order.IOrderService;
 import hello.core.order.OrderServiceImpl;
+import hello.core.repository.IMemberRepository;
 import hello.core.repository.MemoryMemberRepositoryImpl;
 
 public class AppConfig {
 
     public IMemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepositoryImpl());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private IMemberRepository memberRepository() {
+        return new MemoryMemberRepositoryImpl();
     }
 
     public IOrderService orderService() {
         return new OrderServiceImpl(
-                new MemoryMemberRepositoryImpl(),
-                new FixedDiscountPolicy()
+                memberRepository(),
+                discountPolicy()
         );
+    }
+
+    public IDiscountPolicy discountPolicy() {
+        return new FixedDiscountPolicy();
     }
 }
