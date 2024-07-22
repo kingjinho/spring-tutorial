36a24039927c1528dc77f54726c0348219dc4296
kingjinho <jinhoshin319@gmail.com>
2024-07-22 21:47:19 +0900

    test: 빈 스코프 초기화, 종료 콜백 인터페이스를 활용한 빈 테스트 케이스 작성

---
+++ core/src/test/java/hello/core/lifecycle/BeanLifecycleTest.java
@@ -0,0 +1,29 @@
+package hello.core.lifecycle;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifecycleTest {
+
+    @Test
+    public void lifecycleTest() {
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifecycleConfig.class);
+        NetworkClient bean = ac.getBean(NetworkClient.class);
+        ac.close();
+    }
+
+    @Configuration
+    static class LifecycleConfig {
+        @Bean
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://hello.dev");
+            return networkClient;
+        }
+    }
+
+}
